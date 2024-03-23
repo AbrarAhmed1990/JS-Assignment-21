d958b69cb8893bae6e392e9d70cdfcf4b348797b
AbrarAhmed1990 <appdev1990w@gmail.com>
2024-03-22 18:09:40 -0700

    Js Assignment-21

---
+++ app.js
@@ -0,0 +1,60 @@
+// <------Chapter 21 START------>
+
+// Q no 1
+
+// var firstName = prompt("Enter your first name:");
+
+// var lastName = prompt("Enter your last name:");
+
+// var fullName = firstName + " " + lastName;
+
+// alert("Hello, " + fullName + "! Welcome!");
+
+// Q no 2
+
+// var favoriteModel = prompt("What is your favorite mobile phone model?");
+
+// var inputLength = favoriteModel.length;
+
+// document.write("My favorite phone is: "+ favoriteModel +"<br>Length of string: "+ inputLength);
+
+// Q no 3
+
+// var word = "Pakistani";
+
+// var flag = false;
+// for (var i = 0; i < word.length; i++) {
+//   if (word[i] === "n") {
+//     index = i;
+//     flag = true;
+//   }
+// }
+
+// if (flag === true) {
+//   document.write(
+//     "The index 'n' in the word 'Pakistani' is: " + index
+//   );
+// } else alert("Not Found!");
+
+// Q no 4
+
+// var word = "Hello World";
+
+// var flag = false;
+// for (var i = 0; i < word.length; i++) {
+//   if (word[i] === "l") {
+//     index = i;
+//     flag = true;
+//   }
+// }
+
+// if (flag === true) {
+//   document.write(
+//     "String:"+word+"<br>The index 'l': " + index
+//   );
+// } else alert("Not Found!");
+
+// Q no 5
+
+
+// <------Chapter 21 END------>
